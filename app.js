edeffd74355cfe73214a834762842ec5f26a8e68
Revati <revati.v.bordawekar@gmail.com>
2022-12-10 14:10:50 -0800

    implemented add to cart functionality

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
 // Fetching elements from the DOM
 
 const totalPrice = document.getElementById('total-price');
-const products = document.getElementsByClassName('.products');
-const productContainer = document.getElementsByClassName('.product-container');
-const title = document.getElementsByClassName('.title');
-const productDetails = document.getElementsByClassName('.product-details');
-const productImage = document.getElementsByClassName('.prod-img');
-const price = document.getElementsByClassName('.price');
-const buy = document.getElementById('buy');
+const products = document.getElementById('products');
+const fragment = document.createDocumentFragment();
+let sum = 0;
+// fetch the products from the API
+window.onload = function fetchProducts(){
+    let url = "https://fakestoreapi.com/products?limit=5"
+   
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        let productList = data;
+     productList.forEach((product) => {
+        const title = document.createElement('p');
+        const image = document.createElement('img');
+        const price = document.createElement('p');
+        const buy = document.createElement('button');
+    
+        title.textContent = product.title;
+        image.src = product.image;
+        image.classList.add('img');
+        price.textContent = product.price;
+        buy.textContent = 'Buy';
+        
+        buy.onclick = (e) => {
+            e.preventDefault();
+            sum += +price.textContent;
+            totalPrice.textContent = sum.toFixed(2);
+        }
+        fragment.append(title,image, price, buy);
+        
+        
+     })
+     products.appendChild(fragment);
 
+     
+   
+     
+    });
+    
+}
+
+
+
+    
+     
 
 
 
