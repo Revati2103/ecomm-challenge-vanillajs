0c9c5474662c7b87c30d2e051646d8224ce4e6e1
Revati <revati.v.bordawekar@gmail.com>
2022-12-10 07:25:29 -0800

    layout ready

---
+++ app.js
@@ -0,0 +1,15 @@
+// Fetching elements from the DOM
+
+const totalPrice = document.getElementById('total-price');
+const products = document.getElementsByClassName('.products');
+const productContainer = document.getElementsByClassName('.product-container');
+const title = document.getElementsByClassName('.title');
+const productDetails = document.getElementsByClassName('.product-details');
+const productImage = document.getElementsByClassName('.prod-img');
+const price = document.getElementsByClassName('.price');
+const buy = document.getElementById('buy');
+
+
+
+
+
