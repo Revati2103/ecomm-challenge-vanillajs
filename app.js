13e70057f16291c9331f1847d5d105b335208bbf
Revati <revati.v.bordawekar@gmail.com>
2022-12-12 11:51:48 -0800

    updated calculate price functionality

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
-// Fetching elements from the DOM
-
 const totalPrice = document.getElementById('total-price');
 const products = document.getElementById('products');
 const fragment = document.createDocumentFragment();
+let searchInput = document.getElementById('search');
 let sum = 0;
-// fetch the products from the API
-window.onload = function fetchProducts(){
-    let url = "https://fakestoreapi.com/products?limit=5"
-   
-    fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-        let productList = data;
-     productList.forEach((product) => {
-        const title = document.createElement('p');
-        const image = document.createElement('img');
-        const price = document.createElement('p');
-        const buy = document.createElement('button');
-    
-        title.textContent = product.title;
-        image.src = product.image;
-        image.classList.add('img');
-        price.textContent = product.price;
-        buy.textContent = 'Buy';
-        
-        buy.onclick = (e) => {
-            e.preventDefault();
-            sum += +price.textContent;
-            totalPrice.textContent = sum.toFixed(2);
-        }
-        fragment.append(title,image, price, buy);
-        
-        
-     })
-     products.appendChild(fragment);
+let productList = [];
+let filteredProductList = [];
 
-     
+document.addEventListener("DOMContentLoaded", () => {
+    getProducts(); 
+})
    
-     
-    });
+   function getProducts(){
+	fetch("https://fakestoreapi.com/products?limit=5")
+	.then((res) => {
+        return res.json();
+	})
+	.then((data) => {
+	productList = data;
+	products.innerHTML = getHTML(data);
     
+	}) 
+
 }
 
+function getHTML(data){
+    return data
+    .map(({title, image, price}) => generateHTML(title, image, price))
+    .join('');
+}
 
+function generateHTML(title, image, price){
+    let productDetails = `<div class="product">
+    <p class= "title"> ${title}</p>
+    <img src= "${image}" class="img">
+    <p class="price"> $${+price.toFixed(2)}</p>
+    <button id="buy" onClick="calculateTotalPrice(${price})">Buy</button>
+    </div>`; 
 
-    
+    return productDetails;  
      
-
-
-
-
+}
+const calculateTotalPrice = (price) => {
+sum += price;
+totalPrice.textContent = sum;
+}
+  
+searchInput.addEventListener('keyup', function(e){
+    const currentword = e.target.value.toLowerCase();
+    const filteredData= productList.filter(prod => prod.title.toLowerCase().includes(currentword));
+    products.innerHTML = filteredData.length ? getHTML(filteredData) : getHTML(productList);
+});
